06d931b10a11bcf207a0d3a4c7457517999dad37
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-02-03 23:09:31 +0530

    :sparkles: completed signup

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,76 +1,101 @@
-import admin from './firebase';
+import { admin, client } from "./firebase";
 
 export const resolvers = {
-    Query: {
-        async getUsers() {
-            try {
-                let allUsers = [];
-                let users = await admin
-                    .firestore()
-                    .collection('users')
-                    .get();
-                users.forEach(user => allUsers.push(user.data()));
-                return allUsers;
-            } catch (error) {
-                return error;
-            }
-        },
-        async getUser(_root, { id }) {
-            try {
-                let user = await admin
-                    .firestore()
-                    .collection('users')
-                    .doc(id)
-                    .get();
-                // console.log(user);
-                return user.data();
-            } catch (error) {
-                return error;
-            }
-        },
-        async getBookings() {
-            try {
-                let allBookings = [];
-                let bookings = await admin
-                    .firestore()
-                    .collection('bookings')
-                    .get();
-                bookings.forEach(
-                    booking =>
-                        console.log(booking.data()) ||
-                        allBookings.push(booking.data().booking)
-                );
-                console.log(allBookings);
-                return allBookings;
-            } catch (error) {
-                return error;
-            }
-        },
-        async getBooking(_root, { id }) {
-            try {
-                let booking = await admin
-                    .firestore()
-                    .collection('bookings')
-                    .doc(id)
-                    .get();
-                return booking.data();
-            } catch (error) {
-                return error;
-            }
-        },
+  Query: {
+    async getUsers() {
+      try {
+        let allUsers = [];
+        let users = await admin
+          .firestore()
+          .collection("users")
+          .get();
+        users.forEach(user => allUsers.push(user.data()));
+        return allUsers;
+      } catch (error) {
+        return error;
+      }
     },
-    Mutation: {
-        async addBooking(_root, { booking }) {
-            try {
-                let _booking = await admin
-                    .firestore()
-                    .collection('bookings')
-                    .doc()
-                    .set({ booking });
-                return booking;
-            } catch (error) {
-                return error;
-            }
-        },
+    async getUser(_root, { uid }) {
+      try {
+        let user = await admin
+          .firestore()
+          .collection("users")
+          .doc(uid)
+          .get();
+        return user.data();
+      } catch (error) {
+        return error;
+      }
     },
+    async getBookings() {
+      try {
+        let allBookings = [];
+        let bookings = await admin
+          .firestore()
+          .collection("bookings")
+          .get();
+        bookings.forEach(
+          booking =>
+            console.log(booking.data()) || allBookings.push(booking.data())
+        );
+        console.log(allBookings);
+        return allBookings;
+      } catch (error) {
+        return error;
+      }
+    },
+    async getBookingsForUser(_root, { user_id }) {
+      try {
+        let bookings = [];
+        let data = await admin
+          .firestore()
+          .collection("bookings")
+          .get();
+        data.forEach(b => {
+          if (b.data().user_id === user_id) {
+            bookings.push(b.data());
+          }
+        });
+        return bookings;
+      } catch (error) {
+        return error;
+      }
+    }
+  },
+  Mutation: {
+    async addBooking(_root, { booking }) {
+      try {
+        await admin
+          .firestore()
+          .collection("bookings")
+          .doc()
+          .set(booking);
+        return booking;
+      } catch (error) {
+        return error;
+      }
+    },
+    async signUp(_root, { user }) {
+      try {
+        let userDetails = {};
+        let _user = await client
+          .auth()
+          .createUserWithEmailAndPassword(user.email, user.password);
+        userDetails.uid = _user.user.uid;
+        userDetails.name = user.username;
+        userDetails.email = _user.user.email;
+        userDetails.bookings = null;
+        await admin
+          .firestore()
+          .collection("users")
+          .doc(userDetails.uid)
+          .set(userDetails);
+        userDetails.token = await _user.user.getIdToken(true);
+        return userDetails;
+      } catch (error) {
+        return error;
+      }
+    }
+    // @todo add login functionality
+  }
 };
