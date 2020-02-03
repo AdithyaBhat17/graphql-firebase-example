4c887a89927f649e798653ba8e3ead14b6c53df3
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-02-03 23:09:58 +0530

    :sparkles: client side firebase for auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,22 @@
-import admin from 'firebase-admin';
+require("dotenv").config();
+import admin from "firebase-admin";
+import firebase from "firebase";
 
-let serviceAccount = require('../graphql-e7610-firebase-adminsdk-s8wzt-7bb2a7c203.json');
+const serviceAccount = require("../graphql-e7610-firebase-adminsdk-s8wzt-7bb2a7c203.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://graphql-e7610.firebaseio.com',
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.databaseURL
 });
 
-export default admin;
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  appId: process.env.appId
+};
+
+let client = firebase.initializeApp(firebaseConfig);
+
+export { admin, client };
